d0627088f62666325371e3e5967f259aa94eac79
ellie-klm <ellie.klm@kakaocorp.com>
2022-02-04 15:52:15 +0900

    actions-example test fix

--- src/main/java/com/example/githubaction/GithubActionApplication.java
+++ src/main/java/com/example/githubaction/GithubActionApplication.java
@@ -1,9 +1,9 @@
 package com.example.githubaction;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class GithubActionApplication {
 
     public static void main(String[] args) {
